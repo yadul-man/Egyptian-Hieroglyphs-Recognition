0379a558bca725b25d2eec95cf19e84d7eb5fbc1
yadul-man <95302854+yadul-man@users.noreply.github.com>
2022-10-27 22:10:37 +0530

    Add files via upload

---
+++ Translator/js/script.js
@@ -0,0 +1,19 @@
+let filter = 'abcdefghijklmnopqrstuvwxyz';
+function showHieroglyph(){
+	$('#hieroglyph').html('');
+	var str = $('#name').val();
+	str = str.toLowerCase();
+	for (let i = 0; i < str.length; i++) {
+		if (filter.indexOf(str[i]) !== -1) {
+			if (window.matchMedia("(orientation: portrait)").matches) {
+				$('#hieroglyph').append(`<p><img src="img/${str[i]}.png" style="width:80px"></p>`);
+			} else{
+				$('#hieroglyph').append(`<img src="img/${str[i]}.png" style="width:80px">`);
+			}
+		}
+	}
+}
+
+window.onresize = function (event) {
+	showHieroglyph();
+}
